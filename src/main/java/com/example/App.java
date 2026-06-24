d74978dae0e7148ece19014981bcb71c76fe649b
müller-luc <0232608509@uni.lu>
2026-06-24 19:27:52 +0200

    Enhance weather forecast app with averages and filtering
    Added functionality to calculate average temperature and weather score.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,6 +10,10 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter city: ");
         String city = scanner.nextLine();
+        List<Double> temperature = new ArrayList<>();
+        WeatherUtils weatherUtils = new WeatherUtils();
+        List<String> descriptions = new java.util.ArrayList<>();
+        String score = weatherUtils.weatherScore(descriptions);
 
         WeatherApi api = new WeatherApi();
         WeatherParser parser = new WeatherParser();
@@ -16,14 +21,27 @@ public class App {
         String json = api.getForecast(city);
         List<WeatherForecast> forecasts = parser.parse(json);
 
-        for (WeatherForecast f : forecasts) {
-            System.out.println("At " + f.time() + " it will be " +
-                    f.temperature() + "°C and " + f.description());
-        }
+        //filter forcasts to daytime only
+        List<WeatherForecast> forecasts_filtered_daytime = weatherUtils.filtertoDaytime(forecasts);
 
+        for (WeatherForecast f : forecasts_filtered_daytime) {
+                    temperature.add(f.temperature());
+                    descriptions.add(f.description());
+        }
+        int avg_temperature = weatherUtils.setAverageTemperature(temperature);
+        
+        System.out.println("================================");
+        System.out.println("WEATHER FORECAST FOR " + city.toUpperCase());
+        System.out.println("================================");
+        System.out.println();
+        System.out.println("Average  Day Temp: " + avg_temperature +"°C");
+        System.out.println("Average Day Time Sky: " + score);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("================================");
+        System.out.println("=            ENDING            =");
+        System.out.println("================================");
         scanner.close();
     }
 }
-
-
-
